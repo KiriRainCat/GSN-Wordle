27f8b303c39c3abbcc6fb4830b8aaf98dbf493f5
KiriRainCat <2287990465@qq.com>
2024-03-17 16:22:12 +0800

    ✨ feat: 游戏页代办，测试每日单词的获取

--- src/views/GameView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div>This is the game page</div>
-</template>
-
-<style scoped lang="scss"></style>
---
+++ src/views/game/GameView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { getWordOfTheDay } from '@/pkg/services/api'
+import type { Word } from '@/pkg/services/api_types'
+import { onBeforeMount, ref } from 'vue'
+
+const word = ref<Word>()
+onBeforeMount(async () => {
+  word.value = await getWordOfTheDay()
+})
+</script>
+
+<template>
+  <div class="mt-[20%] flex flex-col items-center">
+    <div>The game page, still...</div>
+
+    <div class="text-xl font-bold">W.I.P</div>
+
+    <ul>
+      <li>UI</li>
+      <li>Logic</li>
+    </ul>
+
+    <div class="text-lg font-bold">Word of the day: {{ word?.word }}</div>
+
+    <var-button @click="() => $router.back()">Go Back</var-button>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
