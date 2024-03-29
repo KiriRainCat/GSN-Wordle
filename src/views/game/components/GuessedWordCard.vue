b0a8a4d1fa84a0724d43d584888b34feb0f88966
KiriRainCat <2287990465@qq.com>
2024-03-29 17:21:27 +0800

    ✨ feat: 更好的支持单词中的空格

--- src/views/game/components/GuessedWordCard.vue
+++ src/views/game/components/GuessedWordCard.vue
@@ -13,17 +13,20 @@ defineProps({
 
 <template>
   <div elevation class="mt-3.5 flex gap-1 font-mono text-lg font-bold">
-    <div
-      v-for="(_, idx) in store.word?.word + '???'"
-      :key="idx"
-      :class="
-        `${store.getLetterColorClass(guess[idx] ?? '!', idx)}` +
-        (guess[idx] ? '' : ' bg-gray-50 !px-[1.3rem] !py-6') +
-        ((store.word?.word + '???')[idx] === '?' ? ' opacity-25' : '')
-      "
-      class="rounded-md border-solid border-zinc-600 border-opacity-40 bg-opacity-75 px-4 py-2.5"
-    >
-      {{ (guess[idx] ?? '').toUpperCase() }}
+    <div v-for="(letter, idx) in store.word?.word + '???'" :key="idx">
+      <div v-if="letter === ' '" class="mx-3"></div>
+
+      <div
+        v-else
+        :class="
+          `${store.getLetterColorClass(guess[idx] ?? '!', idx)}` +
+          (guess[idx] ? '' : ' bg-gray-50 !px-[1.311rem] !py-6') +
+          ((store.word?.word + '???')[idx] === '?' ? ' opacity-25' : '')
+        "
+        class="rounded-md border-solid border-zinc-600 border-opacity-40 bg-opacity-75 px-4 py-2.5"
+      >
+        {{ (guess[idx] ?? '').toUpperCase() }}
+      </div>
     </div>
   </div>
 </template>
